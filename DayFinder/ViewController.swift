475161f05e76ad6bccf9713731cf23e8be32c0d6
“EdgarsJaudzems” <jaudzems.edgars@inbox.lv>
2021-02-02 10:22:44 +0200

    showing day in results

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -10,31 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var dayTextField: UITextField!
-    
     @IBOutlet weak var monthTextField: UITextField!
-    
     @IBOutlet weak var yearTextField: UITextField!
-    
     @IBOutlet weak var resultLabel: UILabel!
-    
     @IBOutlet weak var findButton: UIButton!
-    //change from find to clear
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        findButton.layer.cornerRadius = 20
     }
 
     @IBAction func findWeekDayTapped(_ sender: Any) {
+                
         
-        // calendar
-        //DateComponents()
-       
-        //
-        // dateComponents.day = daytextfielt.text
-        // calendar.date(from: dateComponents
-        // DateFormatter()
-        //resultLabel.text = result
+        let calendar = Calendar.current
+
+        var dateComponents = DateComponents()
         
+        dateComponents.day = Int(dayTextField.text ?? "02")
+        dateComponents.month = Int(monthTextField.text ?? "02")
+        dateComponents.year = Int(yearTextField.text ?? "2021")
+
+        let date = calendar.date(from: dateComponents)
+  
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.dateFormat = "EEEE"
+        let weekday = dateFormatter.string(from: date!)
+
+        if dateComponents.day != nil && dateComponents.month != nil && dateComponents.year != nil  {
+            resultLabel.text = "That is \(weekday)!!!"
+            findButton.setTitle("Clear", for: .normal)
+        } else {
+            resultLabel.text = "Enter a day, month and year!"
+        }
+
     }
     
 }
